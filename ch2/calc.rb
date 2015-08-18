8387b2a213fe8d05adb38cd8b9710389e6dafc74
jwrothchild <joanna@Joannas-MacBook-Pro-2.local>
2015-08-17 21:26:10 -0700

    initializing repo

---
+++ ch2/calc.rb
@@ -0,0 +1,23 @@
+puts 1+2
+
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+# hours in a year
+puts "hours in a year:"
+puts 24 * 365
+
+#minutes in a decade
+puts "minutes in a decade:"
+puts ((60 * 24) * 365) * 10
+
+#age in seconds
+puts "my age in seconds:"
+puts (((60 * 60) * 25) * 365) * 23
---
+++ ch3/strings.rb
@@ -0,0 +1,16 @@
+puts 'Hello World!'
+puts ''
+puts 'good-bye.'
+
+puts 'I like ' + 'apple pie.'
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell!'
